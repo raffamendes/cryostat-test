6b49a114492945b07fdc4e398ed72701d52ee183
Raffael Mendes <rmendes@redhat.com>
2023-06-07 16:23:43 -0300

    Adding scond endpoint

--- src/main/java/com/rmendes/GreetingResource.java
+++ src/main/java/com/rmendes/GreetingResource.java
@@ -13,4 +13,11 @@ public class GreetingResource {
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
+    
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String endpoint() {
+    	return "OK";
+    }
 }

dadc03703e325106369e605270204322044eb081
Raffael Mendes <rmendes@redhat.com>
2023-06-09 12:28:53 -0300

    Changing the endpoint return

--- src/main/java/com/rmendes/GreetingResource.java
+++ src/main/java/com/rmendes/GreetingResource.java
@@ -20,6 +20,6 @@ public class GreetingResource {
     @Path("/ok")
     @Produces(MediaType.TEXT_PLAIN)
     public String endpoint() {
-    	return "okidoki rollout test";
+    	return "okidoki rollout";
     }
 }
